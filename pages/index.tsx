c39dc48e31bfd19976750b44bf0142d50955a78a
Marsel Atniashev <atn@marsel.name>
2021-02-03 12:58:01 +0500

    Make newsletter box lighter

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,7 +69,7 @@ export default function IndexPage() {
                 </ul>
               </div>
               {/* newsletter */}
-              <div className="relative lg:rounded-lg px-6 py-10 bg-indigo-600 overflow-hidden shadow-xl sm:px-12 sm:py-20">
+              <div className="relative lg:rounded-lg px-6 py-10 bg-indigo-500 overflow-hidden shadow-xl sm:px-12 sm:py-20">
                 <div
                   aria-hidden="true"
                   className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0"
@@ -82,12 +82,12 @@ export default function IndexPage() {
                     viewBox="0 0 1463 360"
                   >
                     <path
-                      className="text-indigo-500 text-opacity-40"
+                      className="text-indigo-300 text-opacity-40"
                       fill="currentColor"
                       d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
                     />
                     <path
-                      className="text-indigo-700 text-opacity-40"
+                      className="text-indigo-400 text-opacity-40"
                       fill="currentColor"
                       d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
                     />
