9e10acc43c149ee6ea53f45dcb64d68974c9e517
Marsel Atniashev <atn@marsel.name>
2021-05-30 14:58:15 +0500

    Fix code blocks on mobile
    Duct taped by setting an absolute width of the code block in JS.

--- components/code/index.tsx
+++ components/code/index.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import { useEffect, useRef } from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
 
+const ARTICLE_DESKTOP_WIDTH = 655;
+
 export default function Code({ children, className }) {
+  const preRef = useRef(null);
+
+  useEffect(() => {
+    function fitPre() {
+      if (document.body.clientWidth < ARTICLE_DESKTOP_WIDTH) {
+        preRef.current.style.width = `${document.body.clientWidth - 30}px`;
+      } else {
+        preRef.current.style.width = "auto";
+      }
+    }
+
+    fitPre();
+    window.addEventListener("resize", fitPre, true);
+    return () => window.removeEventListener("resize", fitPre, true);
+  }, []);
+
   const language = className.replace(/language-/, "");
   return (
     <Highlight {...defaultProps} code={children} language={language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: "20px" }}>
+        <pre className={className} style={style} ref={preRef}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
