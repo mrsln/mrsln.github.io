8c4283b90a933bdf5e7eafd5a153a884060cf339
Marsel Atniashev <atn@marsel.name>
2022-04-26 02:49:16 +0500

    Update packages
    Notable updates: next 12, react 18, framer-motion 5.
    Had to re-work code highlighting.
    Removed unified-ui because it wasn't doing anything.

--- components/code/index.tsx
+++ components/code/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
+import theme from "prism-react-renderer/themes/nightOwl";
 
 const ARTICLE_DESKTOP_WIDTH = 655;
 
-export default function Code({ children, className }) {
+const Code = ({ children = "", className }) => {
   const preRef = useRef(null);
 
   useEffect(() => {
@@ -22,18 +23,40 @@ export default function Code({ children, className }) {
 
   const language = className.replace(/language-/, "");
   return (
-    <Highlight {...defaultProps} code={children} language={language}>
+    <Highlight
+      {...defaultProps}
+      theme={theme}
+      code={children.trim()}
+      language={language}
+    >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={style} ref={preRef}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
+        <pre
+          className={`${className} text-left mx-4 my-0 p-2 overflow-scroll`}
+          style={style}
+          ref={preRef}
+        >
+          {tokens.map((line, i) => {
+            const lineProps = getLineProps({ line, key: i });
+            const styledLineProps = {
+              ...lineProps,
+              className: `${lineProps.className} table-row`,
+            };
+            return (
+              <div key={i} {...styledLineProps}>
+                <span className="table-cell text-right pr-4 select-none opacity-50 mr-4">
+                  {i + 1}
+                </span>
+                <span className="table-cell">
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </span>
+              </div>
+            );
+          })}
         </pre>
       )}
     </Highlight>
   );
-}
+};
+export default Code;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { AnimateSharedLayout } from "framer-motion";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "prismjs/themes/prism-tomorrow.css";
 
@@ -11,17 +10,12 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <AnimateSharedLayout>
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <div className="min-h-screen flex">
-          <Component {...pageProps} />
-        </div>
-      </AnimateSharedLayout>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="min-h-screen flex">
+        <Component {...pageProps} />
+      </div>
     </QueryClientProvider>
   );
 }
