82a8b50f3febf1f50b531bc47a935109de354717
Marsel Atniashev <atn@marsel.name>
2022-04-26 01:30:03 +0500

    tailwinded

--- components/code/index.tsx
+++ components/code/index.tsx
@@ -1,31 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/nightOwl";
-
-const Pre = styled.pre`
-  text-align: left;
-  margin: 1em 0;
-  padding: 0.5em;
-  overflow: scroll;
-`;
-
-const Line = styled.div`
-  display: table-row;
-`;
-
-const LineNo = styled.span`
-  display: table-cell;
-  text-align: right;
-  padding-right: 1em;
-  user-select: none;
-  opacity: 0.5;
-  margin-right: 1rem;
-`;
-
-const LineContent = styled.span`
-  display: table-cell;
-`;
 
 const Code = ({ children = "", className }) => {
   const language = className.replace(/language-/, "");
@@ -37,18 +12,30 @@ const Code = ({ children = "", className }) => {
       language={language}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <Pre className={className} style={style}>
-          {tokens.map((line, i) => (
-            <Line key={i} {...getLineProps({ line, key: i })}>
-              <LineNo>{i + 1}</LineNo>
-              <LineContent>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </LineContent>
-            </Line>
-          ))}
-        </Pre>
+        <pre
+          className={`${className} text-left mx-4 my-0 p-2 overflow-scroll`}
+          style={style}
+        >
+          {tokens.map((line, i) => {
+            const lineProps = getLineProps({ line, key: i });
+            const styledLineProps = {
+              ...lineProps,
+              className: `${lineProps.className} table-row`,
+            };
+            return (
+              <div key={i} {...styledLineProps}>
+                <span className="table-cell text-right pr-4 select-none opacity-50 mr-4">
+                  {i + 1}
+                </span>
+                <span className="table-cell">
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </span>
+              </div>
+            );
+          })}
+        </pre>
       )}
     </Highlight>
   );
